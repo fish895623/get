ac4648d523eaac9960b62382bb129363e345417d
fish895623 <dan9909429@gmail.com>
2024-04-29 22:46:44 +0900

    feat: Add initial project files for a simple game

---
+++ source/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "config.h"
+
+int main(int argc, char *argv[]) {
+  std::cout << "Hello, World!" << std::endl;
+  std::cout << "Version: " << CMAKE_PROJECT_VERSION << std::endl;
+}
